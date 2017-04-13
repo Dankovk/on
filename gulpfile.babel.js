135506c3cf1fa41f9ea154c94e376278bd45ac3b
Jeff Walter <jeffinmotion@gmail.com>
2017-04-13 13:06:15 -0400

    chore: eslint + eslint adjustments for gulpfile
    eslint runs as part of: npm run lint

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import es from 'event-stream';
 import merge from 'merge-stream';
 import gulp from 'gulp';
 import del from 'del';
@@ -11,22 +10,21 @@ import jsonTransform from 'gulp-json-transform';
 import transform from 'gulp-transform';
 import R from 'ramda';
 import _ from 'lodash/fp';
-import util from 'util';
 import webpack from 'webpack';
 import gutil from 'gulp-util';
-
+// import { trace } from 'lib/debug';
 
 import { CheckerPlugin } from 'awesome-typescript-loader';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-import { attributes } from './lib/attributes.js';
+import { attributes } from './lib/attributes';
 
 
 const browserSync = bs.create();
 
 // Atoms, molecules
-const destPath = 'dist'
+const destPath = 'dist';
 const srcPath = 'src';
 const atomPath = 'atoms';
 const moleculePath = 'molecules';
@@ -42,28 +40,20 @@ const moleculeDestPath = path.join(componentDestPath, moleculePath);
 
 const theatrePackagePath = path.join('packages', 'theatre');
 const theatreDestPath = path.join(destPath, 'theatre');
-const componentStaticTheatreDestPath = path.join(theatreDestPath, 'components');
+// const componentStaticTheatreDestPath = path.join(theatreDestPath, 'components');
 const theatreStyleDestPath = path.join(theatreDestPath, 'style');
-const atomStaticTheatreDestPath = path.join(componentStaticTheatreDestPath, atomPath);
-const moleculeStaticTheatreDestPath = path.join(componentStaticTheatreDestPath, moleculePath);
+// const atomStaticTheatreDestPath = path.join(componentStaticTheatreDestPath, atomPath);
+// const moleculeStaticTheatreDestPath = path.join(componentStaticTheatreDestPath, moleculePath);
 
 // Theatre
 const theatrePath = 'theatre';
 // const theatreAssetPath = path.join(theatrePath, '/**/*.{js,css,html,ts}');
 const theatreAssetPath = path.join(theatrePackagePath, 'src/*.json');
-const theatreStylePath = path.join(theatrePath, '/style');
+// const theatreStylePath = path.join(theatrePath, '/style');
 const theatreTemplatePath = path.join(theatrePath, '/templates');
-const theatreStyleSrcPath = path.join(theatreStylePath, '/**/*.scss');
+// const theatreStyleSrcPath = path.join(theatreStylePath, '/**/*.scss');
 
 const theatreStaticTemplate = 'static.hbs';
-
-
-
-const trace = R.curry(function(tag, x) {
-	console.log(util.inspect(x, false, null));
-	return x;
-});
-
 
 /*
  * Watch functions
@@ -75,6 +65,7 @@ const trace = R.curry(function(tag, x) {
  *     the first arg: a string representing a blob of the files to watch
  *     the remaining args, functions to be placed in a gulp.series
  */
+ // eslint-disable-next-line no-unused-vars
 const watchSeriesFunc = (...args) => () => gulp.watch(R.head(args), gulp.series(R.tail(args)));
 
 /*
@@ -83,6 +74,7 @@ const watchSeriesFunc = (...args) => () => gulp.watch(R.head(args), gulp.series(
  *     the first arg: a string representing a blob of the files to watch
  *     the remaining args, functions to be placed in a gulp.series WITH browserSyncReload appended
  */
+// eslint-disable-next-line no-unused-vars
 const watchFuncBS = (...args) =>
 	(R.equals(R.last(args), browserSyncReload)) ?
 		watchSeriesFunc(...args) :
@@ -94,6 +86,7 @@ const watchFuncBS = (...args) =>
  *     the first arg: the object to merge into
  *     the second arg: the object to be merged into the first argument
  */
+// TODO: Using recursion, not optimized in JS -- alternatives?
 const mergeOmitNil = R.curryN(2, R.compose(
 	_.omitBy(R.isNil),
 	R.mergeWith((l, r) => _.isObject(r) ? mergeOmitNil(l, r) : r),
@@ -101,30 +94,30 @@ const mergeOmitNil = R.curryN(2, R.compose(
 
 
 function getFolders(dir, excludes, obj) {
-	let folders = fs.readdirSync(dir)
-		.filter((file) => {
-			return _.defaultTo([], excludes).indexOf(file) < 0 && fs.statSync(path.join(dir, file)).isDirectory();
-		});
+	const folders = fs.readdirSync(dir)
+		.filter((file) =>
+			_.defaultTo([], excludes).indexOf(file) < 0 && fs.statSync(path.join(dir, file)).isDirectory()
+		);
 
 	return (obj !== true) ? folders :
-		_.map(folder => {
-			return {
+		_.map((folder) =>
+			({
 				name: folder,
 				base: dir
-			}
-		}, folders);
+			})
+		, folders);
 }
 
 /*
  * Task: Clean the project
  */
-const clean = () => del([ destPath ]);
+const clean = () => del([destPath]);
 export { clean };
 
-const cleanComponents = () => del([ componentDestPath ]);
+const cleanComponents = () => del([componentDestPath]);
 export { cleanComponents };
 
-const cleanTheatre = () => del([ theatreDestPath ]);
+const cleanTheatre = () => del([theatreDestPath]);
 export { cleanTheatre };
 
 
@@ -144,7 +137,8 @@ export { registerHbsHelpers };
  */
 function mergeStates(data, file) {
 	return R.when(
-		() => !R.equals(file.relative, 'default.json'), mergeOmitNil(require(file.base + '/default.json'))
+		// eslint-disable-next-line global-require, import/no-dynamic-require
+		() => !R.equals(file.relative, 'default.json'), mergeOmitNil(require(`${file.base}/default.json`))
 	)(data);
 }
 
@@ -163,21 +157,22 @@ function mergeStatesForTemplate(data, file) {
  */
 export function atoms() {
 	return merge(
-		getFolders(atomSrcPath).map((folder) => {
-			return gulp.src(path.join(atomSrcPath, folder, '/states/*.json'))
+		getFolders(atomSrcPath).map((folder) =>
+			gulp.src(path.join(atomSrcPath, folder, '/states/*.json'))
 				.pipe(jsonTransform(mergeStatesForTemplate))
-				.pipe(hbs(gulp.src(path.join(atomSrcPath, folder, folder + '.hbs'))))
-				.pipe(gulp.dest(path.join(atomDestPath, folder)));
-				// TODO: create javascript templates?
-		})
+				.pipe(hbs(gulp.src(path.join(atomSrcPath, folder, `${folder}.hbs`))))
+				.pipe(gulp.dest(path.join(atomDestPath, folder)))
+		)
 	);
 }
 
+/*
+ *
+ */
 export function registerAtomPartials(cb) {
-	getFolders(atomSrcPath).map((folder) => {
-		// console.log('register partials');
-		hbs.registerPartial(folder, fs.readFileSync(path.join(atomSrcPath, folder, folder + '.hbs'), 'utf-8'));
-	});
+	getFolders(atomSrcPath).map((folder) =>
+		hbs.registerPartial(folder, fs.readFileSync(path.join(atomSrcPath, folder, `${folder}.hbs`), 'utf-8'))
+	);
 	cb();
 }
 
@@ -186,31 +181,32 @@ export function registerAtomPartials(cb) {
  */
 export function createMolecules() {
 	return merge(
-		getFolders(moleculeSrcPath).map((folder) => {
-			// console.log('create molecules');
-			return gulp.src(path.join(moleculeSrcPath, folder, '/states/*.json'))
+		getFolders(moleculeSrcPath).map((folder) =>
+			gulp.src(path.join(moleculeSrcPath, folder, '/states/*.json'))
 				.pipe(jsonTransform(mergeStatesForTemplate))
-				.pipe(hbs(gulp.src(path.join(moleculeSrcPath, folder, folder + '.hbs'))))
-				.pipe(gulp.dest(path.join(moleculeDestPath, folder)));
-				// TODO: create javascript templates?
-		})
+				.pipe(hbs(gulp.src(path.join(moleculeSrcPath, folder, `${folder}.hbs`))))
+				.pipe(gulp.dest(path.join(moleculeDestPath, folder)))
+		)
 	);
 }
 
+/*
+ *
+ */
 const molecules = gulp.series(registerAtomPartials, createMolecules);
 
-
+/*
+ *
+ */
 export function sassComponents() {
 	return gulp.src(componentStylePath)
 		.pipe(sass({ includePaths: [componentStyleLibPath] }).on('error', sass.logError))
 		.pipe(gulp.dest(componentDestPath));
 }
 
-
 // const componentStaticTheatreDestPath = path.join(destPath, 'theatre/components');
 // const atomStaticTheatreDestPath = path.join(componentStaticTheatreDestPath, atomPath);
 // const moleculeStaticTheatreDestPath = path.join(componentStaticTheatreDestPath, moleculePath);
-
 
 const buildComponents = gulp.parallel(
 	sassComponents,
@@ -224,35 +220,25 @@ export { buildComponents };
  * Theatre
  */
 export function theatreStaticAtoms() {
-
-	// console.log(
-
-	//  _.concat(
-	//    getFolders(atomDestPath, [], true),
-	//    getFolders(moleculeDestPath, [], true)
-	//  )
-
-	// );
-
 	return merge(
 		_.concat(
 			getFolders(atomDestPath, [], true),
 			getFolders(moleculeDestPath, [], true)
 		)
-		.map((folder) => {
-			return gulp.src(path.join(folder.base, folder.name, '*.html'))
-				.pipe(transform((contents, file) => {
-					return JSON.stringify({
+		.map((folder) =>
+			gulp.src(path.join(folder.base, folder.name, '*.html'))
+				.pipe(transform((contents, file) =>
+					JSON.stringify({
 						title: folder.name,
-						description: 'One static template: ' + folder.name,
+						description: `One static template: ${folder.name}`,
 						theatreStyle: path.join(theatreStyleDestPath.substring(4), 'theatre.css'),
-						componentStyle: path.join(file.base.replace(file.cwd, '').substring(5), folder.name + '.css'),
+						componentStyle: path.join(file.base.replace(file.cwd, '').substring(5), `${folder}.css`),
 						component: contents.toString('utf-8')
-					});
-				}))
+					})
+				))
 				.pipe(hbs(gulp.src(path.join(theatreTemplatePath, theatreStaticTemplate))))
-				.pipe(gulp.dest(path.join(folder.base.replace('dist/static', 'dist/theatre'), folder.name)));
-		})
+				.pipe(gulp.dest(path.join(folder.base.replace('dist/static', 'dist/theatre'), folder.name)))
+		)
 	);
 }
 
@@ -271,25 +257,18 @@ const buildTheatreComponents = gulp.parallel(
 );
 export { buildTheatreComponents };
 
-
-
-
+/*
+ *
+ */
 export function serve() {
 	browserSync.init({
-		server: "./dist"
+		server: './dist'
 	});
 }
-
-
-
-
-
-
 
 /*
  * Future: separate Angular app
  */
-
 
 // export function sassTheatre() {
 //  return gulp.src(theatreStyleSrcPath)
@@ -311,7 +290,9 @@ export function browserSyncReload(cb) {
 // const buildTheatre = gulp.parallel(copyTheatre, sassTheatre);
 // export { buildTheatre };
 
-
+/*
+ *
+ */
 const theatreWebpackConfig = {
 	entry: {
 		polyfill: './packages/theatre/src/polyfill.ts',
@@ -331,7 +312,7 @@ const theatreWebpackConfig = {
 
 	resolveLoader: {
 		// An array of directory names to be resolved to the current directory
-		modules: ["node_modules", path.resolve(__dirname, "packages/theatre/node_modules")]
+		modules: ['node_modules', path.resolve(__dirname, 'packages/theatre/node_modules')]
 	},
 
 	module: {
@@ -366,7 +347,7 @@ const theatreWebpackConfig = {
 		new CheckerPlugin(),
 		new HtmlWebpackPlugin({
 			template: path.join(theatrePackagePath, 'src', 'index.html'),
-			chunksSortMode: function (a, b) {  //reverse alphabetical order
+			chunksSortMode: (a, b) => { // reverse alphabetical order
 				if (a.names[0] < b.names[0]) {
 					return 1;
 				}
@@ -382,9 +363,9 @@ const theatreWebpackConfig = {
 
 export function buildTheatre(cb) {
 	// run webpack
-	webpack(theatreWebpackConfig, function(err, stats) {
-		if(err) throw new gutil.PluginError("webpack", err);
-		gutil.log("[webpack]", stats.toString({
+	webpack(theatreWebpackConfig, (err, stats) => {
+		if (err) throw new gutil.PluginError('webpack', err);
+		gutil.log('[webpack]', stats.toString({
 				// output options
 		}));
 		cb();
@@ -401,8 +382,6 @@ export function buildTheatre(cb) {
 // export { theatre };
 const theatre = gulp.series(cleanTheatre, copyTheatre, buildTheatre);
 export { theatre };
-
-
 
 /*
  * Export a default task
