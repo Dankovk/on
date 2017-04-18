1e0c545b5b1f2683e33da4e041e22f8f1b085e93
Jeff Walter <jeffinmotion@gmail.com>
2017-04-18 11:42:53 -0400

    chore: moved theatre development to webpack +  npm script
    affects: @one/theatre

---
+++ packages/theatre/src/vendor.ts
@@ -0,0 +1,17 @@
+import '@angular/animations';
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/core';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/router';
+import 'redux';
+import 'redux-logger';
+import 'redux-observable';
+import '@angular-redux/store';
+import '@angular-redux/form';
+import '@angular-redux/router';
+import 'immutable';
+import 'rxjs';
